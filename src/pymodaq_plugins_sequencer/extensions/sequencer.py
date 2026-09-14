ea2f638df9edfc80a0dd3be1e8d81d34385351e8
Sébastien Weber <seba.weber@gmail.com>
2026-09-14 22:02:21 +0200

    adapted sequencer extension to ExtensionWorker pattern

--- src/pymodaq_plugins_sequencer/extensions/sequencer.py
+++ src/pymodaq_plugins_sequencer/extensions/sequencer.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 import yaml
 
-from pymodaq.utils.h5modules.module_saving import LoggerSaver
+from pymodaq.extensions.extension_worker import ExtensionWorker
+from pymodaq.utils.h5modules.module_saving import LoggerSaver, DataBundle
 from pymodaq_data import DataToExport
 from pymodaq_gui.managers.h5manager import FileAction
 from pymodaq_gui.managers.runner_thread_manager import WorkerThreadManager
+from pymodaq_gui.messenger import messagebox
 from pymodaq_gui.utils import select_file
 from pymodaq_plugins_sequencer.utilities.sequencer.sequence import Sequence
 
@@ -52,34 +54,6 @@ class StatusBarManager:
         pass
 
 
-class SaverWorker(QtCore.QObject):
-    """ Worker in separated thread receiving the data from a DataGenerator
-    and adding them into the enlargeable arrays with the H5file using the
-     LoggerModuleSaver """
-
-    n_saved = QtCore.Signal(int)
-    data_to_save_signal = QtCore.Signal(DataToExport)
-
-
-    def __init__(self, saver: LoggerSaver, parent=None):
-        super().__init__(parent)
-        self.saver = saver
-        self._n_saved = 0
-        self._show_thread = True
-
-        self.data_to_save_signal.connect(self.save_data, QtCore.Qt.ConnectionType.QueuedConnection)
-
-    @QtCore.Slot(DataToExport)
-    def save_data(self, dte: DataToExport):
-        if self._show_thread:
-            print(f'Saving data in Qthread{self.thread()}')
-            self._show_thread = False
-        self.saver.add_data(dte,)
-        self._n_saved += 1
-        self.n_saved.emit(self._n_saved)
-
-
-
 class Sequencer(CustomExt):
     show_h5file_statusbar_widgets = True
     _worker_done = QtCore.Signal()
@@ -91,6 +65,8 @@ class Sequencer(CustomExt):
     ]
 
     def __init__(self, parent: gutils.DockArea, dashboard):
+
+        self.sequence_worker = SequenceWorker(self)
         super().__init__(parent, dashboard, add_toolbar_break=False)
 
         self.sequences: dict[str, Sequence] = {}
@@ -99,9 +75,6 @@ class Sequencer(CustomExt):
         self.status_manager = StatusBarManager(self)
 
         self._module_and_data_saver = LoggerSaver(self)
-        self.saver_worker: SaverWorker = None
-        self._n_emitted = 0
-
         self.setup_ui()
 
         self._current_path: Path = get_set_sequencer_path()
@@ -238,9 +211,9 @@ class Sequencer(CustomExt):
         self.connect_action('load_sequence', lambda: self.load_sequence())
         self.connect_action('save_sequence', lambda: self.save_sequence())
 
-        self.connect_action('start', self.start)
-        self.connect_action('stop', self.stop)
-        self.connect_action('pause', self.pause)
+        self.connect_action('start', self.sequence_worker.start)
+        self.connect_action('stop', self.sequence_worker.stop)
+        self.connect_action('pause', self.sequence_worker.pause)
 
         self.h5_manager.connect_action(FileAction.SHOW_SETTINGS, self.show_settings)
 
@@ -312,33 +285,62 @@ class Sequencer(CustomExt):
     def main_sequence(self) -> Sequence:
         return self.sequences[self.sequence_names[0]]
 
+    def _quit_fun(self) -> bool:
+        if self.sequence_worker.is_running:
+            messagebox(title='Running',
+                       text='The Sequencer is running, first stop it')
+            return False
+        elif self.settings['worker', 'worker_tasks'] > 0:
+            messagebox(title='Running',
+                       text='The Saver is finishing the savings')
+            self.sequence_worker.stop("User prompted a quit of the Application, Stopping the Acquisition")
+            return False
+
+        return True
+
+
+class SequenceWorker(ExtensionWorker):
+
+    def __init__(self, sequencer: Sequencer, parent=None):
+        self._app: Sequencer
+        super().__init__(app=sequencer, parent=parent)
+
+    @property
+    def app(self) -> Sequencer:
+        return self._app
+
+    @property
+    def status_manager(self) -> StatusBarManager:
+        return self.app.status_manager
+
     def save_callback(self, dte: DataToExport):
         self._n_emitted += 1
-        self.saver_worker.data_to_save_signal.emit(dte)
+        self.saver_worker.data_to_save_signal.emit(DataBundle(dte=dte))
 
-    def start(self):
-        self._init_logging()
-        for sequence in self.sequences.values():
-            sequence.set_log_callback(self.saver_worker.save_data if self.is_action_checked('do_log')
+    def _start(self):
+        self.module_and_data_saver.get_set_node(new=True)
+        for sequence in self.app.sequences.values():
+            sequence.set_log_callback(self.save_callback if self.app.is_action_checked('do_log')
                                       else None)
 
         self._n_emitted = 0
 
-        self.set_action_enabled('start', False)
-        self.main_sequence.sequence_finished.connect(self.stopped)
-        self.main_sequence.get_action('start').trigger()
+        self.app.set_action_enabled('start', False)
+        self.app.main_sequence.sequence_finished.connect(self.stopped)
+        self.app.main_sequence.get_action('start').trigger()
 
-    def pause(self):
-        for sequence in self.sequences.values():
+    def _pause(self, do_pause: bool = True):
+        for sequence in self.app.sequences.values():
             sequence.get_action('pause').trigger()
 
-    def stop(self):
-        for sequence in self.sequences.values():
+    def _stop(self, msg: str = None):
+        for sequence in self.app.sequences.values():
             sequence.get_action('stop').trigger()
 
     def stopped(self, msg: str = None):
+        self._running = False
         #1 Stop the emission of data immediately
-        for sequence in self.sequences.values():
+        for sequence in self.app.sequences.values():
             sequence.recursive_disconnect_elts()
 
         #2 terminate the saver worker once its queue is empty
@@ -348,57 +350,11 @@ class Sequencer(CustomExt):
             self._worker_done.connect(self.terminate_worker)
 
         #3 update the GUI
-        self.set_action_checked('pause', False)
-        self.set_action_enabled('start', True)
+        self._app.set_action_checked('pause', False)
+        self._app.set_action_enabled('start', True)
         if msg is not None:
-            self.update_status(msg)
+            self.app.update_status(msg)
             self.status_manager.set_permanent_status(msg)
-
-    def _init_logging(self):
-        try:
-            self._worker_done.disconnect(self.terminate_worker)
-        except TypeError:
-            pass
-        self.module_and_data_saver.h5saver = self.h5saver
-        self.module_and_data_saver.get_set_node(new=True)
-
-        # managing saver worker
-        self.saver_worker = SaverWorker(saver=self.module_and_data_saver,)
-        self.thread_manager.create_thread_for_worker('saver', self.saver_worker)
-        self.saver_worker.n_saved.connect(self.update_worker_ntask)
-        self.thread_manager.start_thread('saver')
-        self.settings['worker', 'worker_running'] = self.thread_manager.get_thread('saver').isRunning()
-
-    @QtCore.Slot(int)
-    def update_worker_ntask(self, n_saved: int):
-        n_tasks = self._n_emitted - n_saved
-        self.settings['worker', 'worker_tasks'] = n_tasks
-
-        if n_tasks == 0:
-            self._worker_done.emit()
-
-    def terminate_worker(self):
-        """ Will terminate/close/stops a few things when the worker is done working"""
-        # stopping things before flushing/closing the file
-        #1 disconnecting the connection to here (fired once)
-        try:
-            self._worker_done.disconnect(self.terminate_worker)
-        except TypeError:
-            pass
-        try: #2 disconnect the data production from the saving
-            self.saver_worker.data_to_save_signal.disconnect(self.saver_worker.save_data)
-        except TypeError:
-            pass
-
-        #3 quit the thread managing the data saving (nothing left in the loop and no more connection)
-        self.thread_manager.exit_worker_thread('saver', delete_worker=True)
-
-        #4 flushing/closing the file to be able to create new groups...
-        self.h5_manager.close_file()
-
-        #5 updating GUI info
-        self.settings['worker', 'worker_running'] = False
-
 
 
 
